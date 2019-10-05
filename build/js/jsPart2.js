0c9e8f8cd54a49c8b093508d6d65cc82beb521c3
Alexander <lotos_toi@mail.ru>
2019-10-05 10:37:12 +1000

    подправил домашку

--- build/js/jsPart2.js
+++ build/js/jsPart2.js
@@ -2,38 +2,40 @@ function jstask2() { //функция для общего меню
     let namberForSwitch = +prompt(' Введите число от 0 до 15')
     let textForSwitch = 'Выводим числа от ' + namberForSwitch + ' до 15:  '
     switch (namberForSwitch) {
-        case 0: console.log(textForSwitch + '0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15')
+        case 0:
+            console.log(namberForSwitch++)
+        case 1:
+            console.log(namberForSwitch++)
+        case 2:
+            console.log(namberForSwitch++)
+        case 3:
+            console.log(namberForSwitch++)
+        case 4:
+            console.log(namberForSwitch++)
+        case 5:
+            console.log(namberForSwitch++)
+        case 6:
+            console.log(namberForSwitch++)
+        case 7:
+            console.log(namberForSwitch++)
+        case 8:
+            console.log(namberForSwitch++)
+        case 9:
+            console.log(namberForSwitch++)
+        case 10:
+            console.log(namberForSwitch++)
+        case 11:
+            console.log(namberForSwitch++)
+        case 12:
+            console.log(namberForSwitch++)
+        case 13:
+            console.log(namberForSwitch++)
+        case 14:
+            console.log(namberForSwitch++)
+        case 15:
+            console.log(namberForSwitch++)
             break;
-        case 1: console.log(textForSwitch + '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15')
-            break;
-        case 2: console.log(textForSwitch + '2 3 4 5 6 7 8 9 10 11 12 13 14 15')
-            break;
-        case 3: console.log(textForSwitch + '3 4 5 6 7 8 9 10 11 12 13 14 15')
-            break;
-        case 4: console.log(textForSwitch + '4 5 6 7 8 9 10 11 12 13 14 15')
-            break;
-        case 5: console.log(textForSwitch + '5 6 7 8 9 10 11 12 13 14 15')
-            break;
-        case 6: console.log(textForSwitch + '6 7 8 9 10 11 12 13 14 15')
-            break;
-        case 7: console.log(textForSwitch + '7 8 9 10 11 12 13 14 15')
-            break;
-        case 8: console.log(textForSwitch + '8 9 10 11 12 13 14 15')
-            break;
-        case 9: console.log(textForSwitch + '9 10 11 12 13 14 15')
-            break;
-        case 10: console.log(textForSwitch + '10 11 12 13 14 15')
-            break;
-        case 11: console.log(textForSwitch + '11 12 13 14 15')
-            break;
-        case 12: console.log(textForSwitch + '12 13 14 15')
-            break;
-        case 13: console.log(textForSwitch + '13 14 15')
-            break;
-        case 14: console.log(textForSwitch + '14 15')
-            break;
-        case 15: console.log(textForSwitch + '15')
-            break;
-        default: console.log(namberForSwitch + ' лежит за пределами указанного диапазона(0..15) или не явлется числом');
+        default:
+            console.log(namberForSwitch + ' лежит за пределами указанного диапазона(0..15) или не явлется числом');
     }
 }
--- build/js/jsPart3.js
+++ build/js/jsPart3.js
@@ -1,30 +1,45 @@
 function jstask3() { //функция для общего меню
 
-    function addition(a, b) { return a + b }       //функция сложения
-    function sbtraction(a, b) { return a - b }     //функция вычитания
-    function multiplication(a, b) { return a * b } //функция умножения
-    function division(a, b) { return a / b }       //функция деления
+    function addition(a, b) {
+        return console.log('Сумма введенных значнией равна  ' + (a+b));
+    } //функция сложения
+    function sbtraction(a, b) {
+        return console.log('Разность введенных значнией равна  ' + (a-b));
+    } //функция вычитания
+    function multiplication(a, b) {
+        return console.log('Произвединие введенных значнией равно  ' + (a*b));
+    } //функция умножения
+    function division(a, b) {
+        return b && !isNaN(a / b) ? console.log('Результат деления введенных значнией равен  ' + a/b): console.log('Вы делите на 0 или ввели нечисловые значения')
+    } //функция деления
 
-    function outPut(operation, arg1, arg2) {   //Функция с тремя переменными
+    function outPut(operation, arg1, arg2) { //Функция с тремя переменными
         switch (operation) {
-            case 'сложение': console.log('Сумма введенных значнией равна  ' + addition(arg1, arg2));
+            case 'сложение':
+                addition(arg1, arg2)
                 break;
-            case 'вычитание': console.log('Разность введенных значнией равна  ' + sbtraction(arg1, arg2));
+            case 'вычитание':
+                sbtraction(arg1, arg2)
                 break;
-            case 'умножение': console.log('Произведение введенных значнией равно  ' + multiplication(arg1, arg2));
+            case 'умножение':
+                multiplication(arg1, arg2)
                 break;
-            case 'деление': console.log('Результат деления введенных значнией равен  ' + division(arg1, arg2));
+            case 'деление':
+                division(arg1, arg2)
                 break;
-            default: console.log('Команда ' + operation + ' не верна, выедите одну из следующих комнад:сложение, вычитание, умножение, деление')
+            default:
+                console.log('Команда ' + operation + ' не верна, выедите одну из следующих комнад:сложение, вычитание, умножение, деление')
         }
     }
 
+    let namberOperation = prompt('Ведите оперции которую вы хотите выполнить \n сложение \n вычитание \n умножение \n деление')
     let firstVariable = +prompt('Введите значние a');
     let secondVariable = +prompt('Введите значние b');
-    let namberOperation = prompt('Ведите оперции которую вы хотите выполнить \n сложение \n вычитание \n умножение \n деление')
 
-    if (namberOperation === 'сложение' || namberOperation === 'вычитание' || namberOperation === 'умножение' || namberOperation === 'деление') {
+
+    if (namberOperation === 'сложение' || namberOperation === 'вычитание' || namberOperation === 'умножение' || namberOperation === 'деление' || (secondVariable && isNaN(firstVariable/secondVariable))) {
         outPut(namberOperation, firstVariable, secondVariable)
+    } else {
+        console.log('Вы ввели неправильную команду' + namberOperation + ', Введите одну из следующих комнад:сложение, вычитание, умножение, деление')
     }
-    else { console.log('Вы ввели неправильную команду' + namberOperation + ', Введите одну из следующих комнад:сложение, вычитание, умножение, деление') }
 }
